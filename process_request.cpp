f98f6f62f1d44ded9338f29288672f599d38e465
Elena Lungu <miphigen@ox-p1.msk.21-school.ru>
2021-04-08 11:24:15 +0300

    .

--- process_request.cpp
+++ process_request.cpp
@@ -7,7 +7,7 @@ void process_request(bigass_struct &bas, struct sockaddr_in &addr, int sockfd)
     int new_connection = accept(sockfd, (struct sockaddr *)&addr, &sizeofaddr);
     if (new_connection == 0)
     {
-        std::cout << "Connection error\n";
+        std::cout << "error accept()\n";
         return;
     }
 
@@ -18,13 +18,17 @@ void process_request(bigass_struct &bas, struct sockaddr_in &addr, int sockfd)
     int bytes_recvd = recv(new_connection, request, request_buffer_size - 1, 0);
     if (bytes_recvd < 0)
     {
-        fprintf(stderr, "error recv\n");
+        fprintf(stderr, "error recv()\n");
         return;
     }
     request[bytes_recvd] = '\0';
 
-    std::cout << "request:\n" << request << std::endl;
+    //parse_request();
+    //write_responce();
+    //send_responce()
 
+
+    std::cout << "request:\n" << request << std::endl;
     char buffer[256];
     strcat(buffer, "HTTP/1.1 200 OK\n\n");
     strcat(buffer, "Hello 1234");

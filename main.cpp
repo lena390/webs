0549291e5bd28f73d1489c195c31e51004d5d379
tanovs <tanovsyankina@gmail.com>
2021-04-26 15:20:02 +0300

    Just a simple main

--- main.cpp
+++ main.cpp
@@ -6,24 +6,29 @@
 /*   By: atable <atable@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 12:52:22 by atable            #+#    #+#             */
-/*   Updated: 2021/04/25 15:32:18 by atable           ###   ########.fr       */
+/*   Updated: 2021/04/26 15:15:39 by atable           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers.hpp"
 #include "Server.hpp"
 
-Serv fill_server( void )
+Serv fill_server( int port )
 {
-    Serv serv1(8000);
+    Serv serv1(port);
     return serv1;
 }
 
 int main()
 {
-    Server serv(fill_server());
-    serv.initServer();
-    serv.startServer();
-    serv.clearServer();
+    std::vector<Serv> serv;
+    serv.push_back(fill_server(8000));
+    serv.push_back(fill_server(8080));
+    Server server(serv);
+    if (!server.initServer())
+    {
+        server.startServer();
+        // server.clearServer();
+    }
     return 0;
 }

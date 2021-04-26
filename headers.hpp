0db103a15733b5025d30432793e251e6e88946e0
tanovs <tanovsyankina@gmail.com>
2021-04-26 11:20:22 +0300

    Add colors

--- headers.hpp
+++ headers.hpp
@@ -6,29 +6,35 @@
 /*   By: atable <atable@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 21:12:39 by atable            #+#    #+#             */
-/*   Updated: 2021/04/20 21:48:04 by atable           ###   ########.fr       */
+/*   Updated: 2021/04/26 11:19:50 by atable           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADERS_HPP
 # define HEADERS_HPP
 
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <netdb.h>
-#include <time.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <fstream>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include "fcntl.h"
-#include <sys/ioctl.h>
+# include <iostream>
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+# include <netdb.h>
+# include <time.h>
+# include <sys/socket.h>
+# include <unistd.h>
+# include <fstream>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include "fcntl.h"
+# include <sys/ioctl.h>
 
-#include <vector>
-#include <map>
+# include <sstream>
+
+# include <vector>
+# include <map>
+
+# define RED    "\033[31m"
+# define GREEN  "\033[31m"
+# define RESET  "\033[0m"
 
 int print_error(std::string err, int status);
 

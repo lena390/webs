3552e03781cd46a5304bbbc534ae8f03fdf351e6
tanovs <tanovsyankina@gmail.com>
2021-06-28 13:06:02 +0300

    n

--- Request.hpp
+++ Request.hpp
@@ -6,7 +6,7 @@
 /*   By: atable <atable@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 15:30:30 by atable            #+#    #+#             */
-/*   Updated: 2021/06/28 12:46:43 by atable           ###   ########.fr       */
+/*   Updated: 2021/06/28 13:05:43 by atable           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 # define REQUEST_HPP
 
 #include "headers.hpp"
-#include "Response.hpp"
-#include "CGI.hpp"
 
 class Responce;
 class CGI;
